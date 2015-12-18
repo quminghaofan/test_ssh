acd57f18f1a12625508e363f7ef159cadb4ce02e
xychenting <12345678910@qq.com>
2015-12-18 18:06:43 +0800

    ct

--- src/cn/edu/xmu/oneonezero/view/ArtistController.java
+++ src/cn/edu/xmu/oneonezero/view/ArtistController.java
@@ -1,11 +1,13 @@
 package cn.edu.xmu.oneonezero.view;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.xml.crypto.Data;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import cn.edu.xmu.oneonezero.entity.DataDictionary;
 import cn.edu.xmu.oneonezero.entity.User;
 import cn.edu.xmu.oneonezero.service.UserService;
 
@@ -20,7 +22,9 @@ public class ArtistController {
 	public String artistApply(HttpServletRequest request){
 		User user=(User)request.getSession().getAttribute("user");
 		user.setBankCardAccount(request.getParameter("account"));
-		user.setRole("预备艺术家");
+		DataDictionary dataDictionary=user.getRole();;
+		dataDictionary.setName("预备艺术家");
+		user.setRole(dataDictionary);
 		user.setRealName(request.getParameter("realname"));
 		userService.updateUser(user);
 		return "redirect:/init/home";
