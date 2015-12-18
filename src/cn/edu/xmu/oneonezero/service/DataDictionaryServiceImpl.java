809c653ff0b1d18fa77c70f1eb073ed1da20a2bd
Liuyubao <liuyubao@liuyubaodeMacBook-Pro.local>
2015-12-18 19:26:06 +0800

    lab

--- src/cn/edu/xmu/oneonezero/service/DataDictionaryServiceImpl.java
+++ src/cn/edu/xmu/oneonezero/service/DataDictionaryServiceImpl.java
@@ -6,6 +6,9 @@ import cn.edu.xmu.oneonezero.entity.DataDictionary;
 public class DataDictionaryServiceImpl implements DataDictionaryService{
 	private DataDictionaryDao dataDictionaryDao;
 
+	public void setDataDictionaryDao(DataDictionaryDao dataDictionaryDao) {
+		this.dataDictionaryDao = dataDictionaryDao;
+	}
 	@Override
 	public DataDictionary getDataDictionaryByName(String name) {
 		return dataDictionaryDao.getDataDictionaryByName(name);
