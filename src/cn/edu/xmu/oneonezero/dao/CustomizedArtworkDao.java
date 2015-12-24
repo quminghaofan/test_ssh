8a29da0a8a92fdd7ec926d93f6017b7a072c41f2
Liuyubao <liuyubao@liuyubaodeMacBook-Pro.local>
2015-12-24 21:11:47 +0800

    lyb

--- src/cn/edu/xmu/oneonezero/dao/CustomizedArtworkDao.java
+++ src/cn/edu/xmu/oneonezero/dao/CustomizedArtworkDao.java
@@ -8,6 +8,10 @@ public interface CustomizedArtworkDao {
 
 	public List<CustomizedArtwork> getAllCustomizedArtworks();//获取所有定制艺术品
 	
+	public List<CustomizedArtwork> getCustomizedArtworksByVagueArtworkNamePageNumber(Long artworkTypeId, String artName, int start,int num);//根据艺术品类型id，模糊艺术品名称，页码，每页数量 分页获取定制艺术品，返回list
+
+	public void insertCustomizedArtwork(CustomizedArtwork customizedArtwork);//添加定制艺术品
+
 	
 
 }
