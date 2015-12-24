c4c53f7d9a21386574b91df817dc12695ea34c06
quminghaofan <1149477151@qq.com>
2015-12-24 22:51:00 +0800

    aaa

--- WebContent/sql/成品展品.sql
+++ WebContent/sql/成品展品.sql
@@ -104,96 +104,96 @@ insert into artwork(artworkType,artworkDescription,author
 insert into artwork(artworkType,artworkDescription,author
 ,isExist,name,picUrl,canSell,price,sellState,ownerId,typeId) 
            values('艺术品商品','这是艺术品成品','艺术家1',
-                1,'展品1','../images/pic.jpg',1,500,1,14,10);
+                0,'展品1','../images/pic.jpg',1,500,1,14,10);
 insert into artwork(artworkType,artworkDescription,author
 ,isExist,name,picUrl,canSell,price,sellState,ownerId,typeId) 
            values('艺术品商品','这是艺术品成品','艺术家1',
-                1,'展品2','../images/pic.jpg',1,500,1,15,11);
+                0,'展品2','../images/pic.jpg',1,500,1,15,11);
 insert into artwork(artworkType,artworkDescription,author
 ,isExist,name,picUrl,canSell,price,sellState,ownerId,typeId) 
            values('艺术品商品','这是艺术品成品','艺术家1',
-                1,'展品3','../images/pic.jpg',1,500,1,16,12);
+                0,'展品3','../images/pic.jpg',1,500,1,16,12);
 insert into artwork(artworkType,artworkDescription,author
 ,isExist,name,picUrl,canSell,price,sellState,ownerId,typeId) 
            values('艺术品商品','这是艺术品成品','艺术家1',
-                1,'展品4','../images/pic.jpg',1,500,1,17,13);
+                0,'展品4','../images/pic.jpg',1,500,1,17,13);
 insert into artwork(artworkType,artworkDescription,author
 ,isExist,name,picUrl,canSell,price,sellState,ownerId,typeId) 
            values('艺术品商品','这是艺术品成品','艺术家1',
-                1,'展品5','../images/pic.jpg',1,500,1,18,14);
+                0,'展品5','../images/pic.jpg',1,500,1,18,14);
 insert into artwork(artworkType,artworkDescription,author
 ,isExist,name,picUrl,canSell,price,sellState,ownerId,typeId) 
            values('艺术品商品','这是艺术品成品','艺术家1',
-                1,'展品6','../images/pic.jpg',1,500,1,19,15);
+                0,'展品6','../images/pic.jpg',1,500,1,19,15);
 insert into artwork(artworkType,artworkDescription,author
 ,isExist,name,picUrl,canSell,price,sellState,ownerId,typeId) 
            values('艺术品商品','这是艺术品成品','艺术家1',
-                1,'展品7','../images/pic.jpg',1,500,1,20,16);
+                0,'展品7','../images/pic.jpg',1,500,1,20,16);
 insert into artwork(artworkType,artworkDescription,author
 ,isExist,name,picUrl,canSell,price,sellState,ownerId,typeId) 
            values('艺术品商品','这是艺术品成品','艺术家1',
-                1,'展品8','../images/pic.jpg',1,500,1,21,13);
+                0,'展品8','../images/pic.jpg',1,500,1,21,13);
 insert into artwork(artworkType,artworkDescription,author
 ,isExist,name,picUrl,canSell,price,sellState,ownerId,typeId) 
            values('艺术品商品','这是艺术品成品','艺术家1',
-                1,'展品9','../images/pic.jpg',1,500,1,22,14);
+                0,'展品9','../images/pic.jpg',1,500,1,22,14);
 insert into artwork(artworkType,artworkDescription,author
 ,isExist,name,picUrl,canSell,price,sellState,ownerId,typeId) 
            values('艺术品商品','这是艺术品成品','艺术家1',
-                1,'成品12','../images/pic.jpg',1,500,1,23,11);
+                0,'成品12','../images/pic.jpg',1,500,1,23,11);
 insert into artwork(artworkType,artworkDescription,author
 ,isExist,name,picUrl,canSell,price,sellState,ownerId,typeId) 
            values('艺术品商品','这是艺术品成品','艺术家1',
-                1,'展品10','../images/pic.jpg',1,500,1,24,10);
+                0,'展品10','../images/pic.jpg',1,500,1,24,10);
 insert into artwork(artworkType,artworkDescription,author
 ,isExist,name,picUrl,canSell,price,sellState,ownerId,typeId) 
            values('艺术品商品','这是艺术品成品','艺术家1',
-                1,'展品11','../images/pic.jpg',1,500,1,25,13);
+                0,'展品11','../images/pic.jpg',1,500,1,25,13);
 insert into artwork(artworkType,artworkDescription,author
 ,isExist,name,picUrl,canSell,price,sellState,ownerId,typeId) 
            values('艺术品商品','这是艺术品成品','艺术家1',
-                1,'展品13','../images/pic.jpg',1,500,1,14,10);
+                0,'展品13','../images/pic.jpg',1,500,1,14,10);
 insert into artwork(artworkType,artworkDescription,author
 ,isExist,name,picUrl,canSell,price,sellState,ownerId,typeId) 
            values('艺术品商品','这是艺术品成品','艺术家1',
-                1,'展品14','../images/pic.jpg',1,500,1,15,16);
+                0,'展品14','../images/pic.jpg',1,500,1,15,16);
 insert into artwork(artworkType,artworkDescription,author
 ,isExist,name,picUrl,canSell,price,sellState,ownerId,typeId) 
            values('艺术品商品','这是艺术品成品','艺术家1',
-                1,'展品3','../images/pic.jpg',1,500,1,16,10);
+                0,'展品3','../images/pic.jpg',1,500,1,16,10);
 insert into artwork(artworkType,artworkDescription,author
 ,isExist,name,picUrl,canSell,price,sellState,ownerId,typeId) 
            values('艺术品商品','这是艺术品成品','艺术家1',
-                1,'展品15','../images/pic.jpg',1,500,1,17,11);
+                0,'展品15','../images/pic.jpg',1,500,1,17,11);
 insert into artwork(artworkType,artworkDescription,author
 ,isExist,name,picUrl,canSell,price,sellState,ownerId,typeId) 
            values('艺术品商品','这是艺术品成品','艺术家1',
-                1,'展品16','../images/pic.jpg',1,500,1,18,10);
+                0,'展品16','../images/pic.jpg',1,500,1,18,10);
 insert into artwork(artworkType,artworkDescription,author
 ,isExist,name,picUrl,canSell,price,sellState,ownerId,typeId) 
            values('艺术品商品','这是艺术品成品','艺术家1',
-                1,'展品17','../images/pic.jpg',1,500,1,19,10);
+                0,'展品17','../images/pic.jpg',1,500,1,19,10);
 insert into artwork(artworkType,artworkDescription,author
 ,isExist,name,picUrl,canSell,price,sellState,ownerId,typeId) 
            values('艺术品商品','这是艺术品成品','艺术家1',
-                1,'展品18','../images/pic.jpg',1,500,1,20,13);
+                0,'展品18','../images/pic.jpg',1,500,1,20,13);
 insert into artwork(artworkType,artworkDescription,author
 ,isExist,name,picUrl,canSell,price,sellState,ownerId,typeId) 
            values('艺术品商品','这是艺术品成品','艺术家1',
-                1,'展品19','../images/pic.jpg',1,500,1,21,10);
+                0,'展品19','../images/pic.jpg',1,500,1,21,10);
 insert into artwork(artworkType,artworkDescription,author
 ,isExist,name,picUrl,canSell,price,sellState,ownerId,typeId) 
            values('艺术品商品','这是艺术品成品','艺术家1',
-                1,'展品20','../images/pic.jpg',1,500,1,22,10);
+                0,'展品20','../images/pic.jpg',1,500,1,22,10);
 insert into artwork(artworkType,artworkDescription,author
 ,isExist,name,picUrl,canSell,price,sellState,ownerId,typeId) 
            values('艺术品商品','这是艺术品成品','艺术家1',
-                1,'展品21','../images/pic.jpg',1,500,1,23,10);
+                0,'展品21','../images/pic.jpg',1,500,1,23,10);
 insert into artwork(artworkType,artworkDescription,author
 ,isExist,name,picUrl,canSell,price,sellState,ownerId,typeId) 
            values('艺术品商品','这是艺术品成品','艺术家1',
-                1,'展品22','../images/pic.jpg',1,500,1,24,10);
+                0,'展品22','../images/pic.jpg',1,500,1,24,10);
 insert into artwork(artworkType,artworkDescription,author
 ,isExist,name,picUrl,canSell,price,sellState,ownerId,typeId) 
            values('艺术品商品','这是艺术品成品','艺术家1',
-                1,'展品23','../images/pic.jpg',1,500,1,25,15);
+                0,'展品23','../images/pic.jpg',1,500,1,25,15);
