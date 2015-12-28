e07c64e260133ea88bd754b6f42842768f871cd0
quminghaofan <1149477151@qq.com>
2015-12-28 13:46:03 +0800

    aaa

---
+++ WebContent/sql/artworkorder2.sql
@@ -0,0 +1,9 @@
+INSERT INTO `artworkorder` VALUES ('艺术品商品订单', 2, '厦门', '2', 0, '13689898888', '1451223419382', '2015-12-27', '已发货已支付', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 32, 22, NULL);
+INSERT INTO `artworkorder` VALUES ('定制艺术品订单', 3, '厦门大学x', '1', 0, '12345678900', '1451226268516', '2015-12-27', '未支付', '..\\attached\\1451226268341.jpg', 43543, 4324, '生产法收到的v', '未支付', 123, NULL, 32, NULL, 52);
+INSERT INTO `artworkorder` VALUES ('定制艺术品订单', 4, '如果该回家', '1', 0, '12345678900', '1451234323417', '2015-12-28', '已支付未收货', '..\\attached\\1451234322826.jpg', 0, 0, '人GV画就', '第一阶段已支付', 0, NULL, 32, NULL, 54);
+INSERT INTO `artworkorder` VALUES ('艺术品商品订单', 5, '厦门', '2', 1, '13689898888', '1451241567281', '2015-12-28', '已支付未发货', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 13, 1, NULL);
+INSERT INTO `artworkorder` VALUES ('定制艺术品订单', 6, 'efdfg', '2', 0, '12345678900', '1451243541366', '2015-12-28', '未支付', '..\\attached\\1451243541256.jpeg', 0, 0, 'er', '', 0, NULL, 32, NULL, 58);
+INSERT INTO `artworkorder` VALUES ('定制艺术品订单', 7, 'kkl', '2', 0, '12345678900', '1451243777880', '2015-12-28', '未支付', '..\\attached\\1451243777827.jpg', 0, 0, 'jjk', '', 0, NULL, 32, NULL, 59);
+INSERT INTO `artworkorder` VALUES ('定制艺术品订单', 8, 'qweqee', '2', 0, '13123333666', '1451248046953', '2015-12-28', '未支付', '..\\attached\\1451248046874.jpg', 0, 0, 'w s s', '', 0, NULL, 42, NULL, 60);
+INSERT INTO `artworkorder` VALUES ('定制艺术品订单', 9, 'dsaf', '2', 0, '12312312322', '1451254423598', '2015-12-28', '未支付', '..\\attached\\1451254423481.jpg', 0, 0, 'dsfa', '', 0, NULL, 32, NULL, 61);
+INSERT INTO `artworkorder` VALUES ('定制艺术品订单', 10, 'dfsdfd', '2', 0, '13222455664', '1451257675584', '2015-12-28', '未支付', '..\\attached\\1451257675419.jpeg', 0, 0, 'dfsdf', '', 0, NULL, 32, NULL, 62);
